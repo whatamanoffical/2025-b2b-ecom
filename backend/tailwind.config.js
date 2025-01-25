2e5db76658b9a5842f101dbb8d6643cda4ef71d6
whatamanoffical <157410409+whatamanoffical@users.noreply.github.com>
2025-01-25 19:48:36 +0100

    Update tailwind.config.js

--- backend/tailwind.config.js
+++ backend/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/@medusajs/ui/dist/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {},
   presets: [require("@medusajs/ui-preset")],
 };
